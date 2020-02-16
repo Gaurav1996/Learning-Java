d0bb11f8fd3ccfe8ba56f09d1b27ccd69ccc552e
cb-gaurav <gaurav@chargebee.com>
2020-02-16 15:40:07 +0530

    Commiting string equals check file

---
+++ practice/src/main/java/com/gagrawal/practice/quick/StringEqualsCheck.java
@@ -0,0 +1,15 @@
+package com.gagrawal.practice.quick;
+
+/**
+ * @author Gaurav
+ * 19/12/19
+ */
+public class StringEqualsCheck {
+    public static void main(String[] args) {
+        String s1 = null;
+        String s2 = "Hello";
+        if(s2.equals(s1)) {
+            System.out.println("Really??");
+        }
+    }
+}
